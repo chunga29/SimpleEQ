3f6d78e426714a5c31b863e6dcbb7a0e1c2700e9
Adam Chung <adamchung@Adams-MacBook-Pro.local>
2024-01-24 20:06:17 -0800

    customized the graphics

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -152,6 +152,11 @@ struct LookAndFeel : juce::LookAndFeel_V4
                                    float rotaryStartAngle,
                                    float rotaryEndAngle,
                                    juce::Slider&) override;
+    
+    void drawToggleButton (juce::Graphics& g,
+                           juce::ToggleButton& toggleButton,
+                           bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -296,6 +301,8 @@ private:
     
     
     std::vector<juce::Component*> getComps();
+    
+    LookAndFeel lnf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
